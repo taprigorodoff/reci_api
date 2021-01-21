2733ff7d9034c9f78fc0bb90872e5c32a58d97ba
tadomracheff <tadomracheff@gmail.com>
2021-01-21 21:22:20 +0500

    add put and delete recipe ingredient

--- app/resources/Recipe.py
+++ app/resources/Recipe.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask import send_from_directory, jsonify
+from flask import send_from_directory
 from sqlalchemy import exc
 from resources.models import Recipe, RecipeIngredient, Ingredient
 from resources.models import DCategory, DStage, DUnit, DPrepackType
@@ -146,6 +146,75 @@ class RecipeIngredientDetail(Resource):
         ri = RecipeIngredient.query.filter(RecipeIngredient.id == id,
                                            RecipeIngredient.recipe_id == recipe_id).first_or_404()
         return ri.as_json(), 200
+
+    def put(self, recipe_id, id):
+        unit_ids = [cat.id for cat in db.session.query(DUnit.id).all()]  # todo кэш
+        prepack_type_ids = [cat.id for cat in db.session.query(DPrepackType.id).all()]  # todo кэш
+        stage_ids = [cat.id for cat in db.session.query(DStage.id).all()]  # todo кэш
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('ingredient_id', type=int, help='{error_msg}')  # check id
+        parser.add_argument('amount')
+        parser.add_argument('required', type=bool, help='{error_msg}')
+        parser.add_argument('unit_id', type=int, choices=unit_ids, help='Bad choice: {error_msg}')
+        parser.add_argument('prepack_type_id', type=int, required=False, nullable=True, choices=prepack_type_ids,
+                            help='Bad choice: {error_msg}')
+        parser.add_argument('stage_id', type=int, required=False, choices=stage_ids, help='Bad choice: {error_msg}')
+        parser.add_argument('alternative_ids', type=int, required=False, action='append',
+                            help='{error_msg}')  # check id
+
+        args = parser.parse_args()
+
+        ingredient = Ingredient.query.filter(Ingredient.id == args['ingredient_id']).first()
+        if not ingredient:
+            return {
+                       'messages': 'Bad choice for ingredient_id'
+                   }, 400
+        if args['alternative_ids']:
+            for alternative_id in args['alternative_ids']:
+                ingredient = Ingredient.query.filter(Ingredient.id == alternative_id).first()
+
+                if not ingredient:
+                    return {
+                               'messages': 'Bad choice for alternative_ids'
+                           }, 400
+
+        ri = RecipeIngredient.query.filter(RecipeIngredient.id == id).first()
+
+        ri.ingredient_id = args['ingredient_id']
+        ri.amount = args['amount']
+        ri.unit_id = args['unit_id']
+        ri.required = args['required']
+        ri.prepack_type_id = args['prepack_type_id']
+        ri.stage_id = args['stage_id']
+
+        if args['alternative_ids']:
+            for alternative_id in args['alternative_ids']:
+                ri.ingredient_alternatives.append(Ingredient.query.get(alternative_id))
+
+        db.session.add(ri)
+
+        try:
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            db.session.rollback()
+            return {
+                       'messages': e.args
+                   }, 500
+
+        return ri.as_json(), 201
+
+    def delete(self, recipe_id, id):
+        r = RecipeIngredient.query.filter(RecipeIngredient.id == id).first_or_404()
+        db.session.add(r)
+        db.session.delete(r)
+        try:
+            db.session.commit()
+        except exc.SQLAlchemyError as e:
+            db.session.rollback()
+            return {
+                       'messages': e.args
+                   }, 500
 
 
 class RecipeImg(Resource):
